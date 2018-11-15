5762d34a0990ad9e4ef482a048a63f6e5296ee02
Stephan Vits <stephan.vits@gmail.com>
2018-11-15 18:31:39 +0630

    add page.pop() functionality

--- tutorial/svi001/src/pages/users/user/user.ts
+++ tutorial/svi001/src/pages/users/user/user.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-user',
@@ -9,7 +9,9 @@ import { NavParams } from 'ionic-angular';
 export class UserPage implements OnInit {
 	name: string;
 
-	constructor (private navParams: NavParams) {
+	constructor (
+		private navParams: NavParams,
+		private navCtrl: NavController) {
 
 	}
 
@@ -17,4 +19,7 @@ export class UserPage implements OnInit {
 		this.name = this.navParams.get('userName');
 	}
 
+	onGoBack() {
+		this.navCtrl.pop();
+	}
 }
