39a82f001dbc69aade0609734eb6d188c9cf9493
Stephan Vits <stephan.vits@gmail.com>
2018-11-15 18:24:44 +0630

    passing data between pages

--- tutorial/svi001/src/pages/home/home.ts
+++ tutorial/svi001/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsersPage } from '../users/users';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  onGoToUsers() {
+  	this.navCtrl.push(UsersPage);	
+  }
 }
