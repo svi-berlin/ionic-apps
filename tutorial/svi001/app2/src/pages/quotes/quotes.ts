70776eb3be07d2a175514355989d7c19fc686c28
Stephan Vits <stephan.vits@gmail.com>
2018-11-19 19:49:39 +0630

    favorite quotes app initialized

---
+++ tutorial/svi001/app2/src/pages/quotes/quotes.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage implements OnInit {
+	
+	quoteGroup: {category: string, quotes: Quote[], icon: string};
+	
+	constructor(private navParams: NavParams) {}
+	
+	ngOnInit() {
+		this.quoteGroup = this.navParams.data;
+	}
+
+	//ionViewDidLoad() {
+	//	this.quoteGroup = this.navParams.data;
+	//}
+}
