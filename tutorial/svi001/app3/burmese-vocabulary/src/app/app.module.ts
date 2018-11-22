f1e2ec1f7bd997c49a82390ad4e66238fe8085ef
Stephan Vits <stephan.vits@gmail.com>
2018-11-22 12:18:40 +0630

    burmese vocabulary initialized

--- tutorial/svi001/app3/burmese-vocabulary/src/app/app.module.ts
+++ tutorial/svi001/app3/burmese-vocabulary/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LibraryPage } from '../pages/library/library';
+import { VocabularyPage } from '../pages/vocabulary/vocabulary';
 import { FlashCardComponent } from '../components/flash-card/flash-card';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
+    LibraryPage,
+    VocabularyPage,
     FlashCardComponent
   ],
   imports: [
@@ -21,7 +25,9 @@ import { FlashCardComponent } from '../components/flash-card/flash-card';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LibraryPage,
+    VocabularyPage
   ],
   providers: [
     StatusBar,
---
+++ tutorial/svi001/app3/burmese-vocabulary/src/data/vocabulary.interface.ts
@@ -0,0 +1,5 @@
+export interface Vocabulary {
+	id: string;
+	german: string;
+	burmese: string;
+}
