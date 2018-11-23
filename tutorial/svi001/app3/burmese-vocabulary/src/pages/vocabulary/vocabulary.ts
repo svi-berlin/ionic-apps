7a1dfea14dbc0a5f8b68e8016d9f0c4a2535306a
Stephan Vits <stephan.vits@gmail.com>
2018-11-23 15:39:32 +0630

    vocabulary changed

--- tutorial/svi001/app3/burmese-vocabulary/src/pages/vocabulary/vocabulary.ts
+++ tutorial/svi001/app3/burmese-vocabulary/src/pages/vocabulary/vocabulary.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
 import { Vocabulary } from '../../data/vocabulary.interface';
-// import { VocabularyService } from '../../services/vocabulary';
 
 @Component({
   selector: 'page-vocabulary',
@@ -13,37 +11,11 @@ export class VocabularyPage implements OnInit {
 	vocabularyGroup: {category: string, vocabulary: Vocabulary[], icon: string};
 	
 	constructor(
-		private navParams: NavParams,
-		private alertCtrl: AlertController,
-		//private vocabularyService: VocabularyService
+		private navParams: NavParams
 		) {}
 	
 	ngOnInit() {
 		this.vocabularyGroup = this.navParams.data;
 	}
 
-	onAddToFavorite(selectedQuote: Quote) {
-		const alert = this.alertCtrl.create({
-			title: 'Add Quote',
-			subTitle: 'Are you sure?',
-			message: 'Are you sure you want to add the quote?',
-			buttons: [
-				{
-					text: 'yes',
-					handler: () => {
-						this.quotesService.addQuoteToFavorites(selectedQuote);
-						console.log('yes');
-					}
-				},
-				{
-					text: 'no',
-					role: 'cancel',
-					handler: () => {
-						console.log('no');
-					}
-				}
-			]
-		});
-		alert.present();
-	}
 }
