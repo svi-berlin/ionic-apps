35bd015ed832e4b1192d4155aa693d9ab1cab33a
Stephan Vits <stephan.vits@gmail.com>
2018-12-04 10:21:54 +0630

    audio mp3 added

--- tutorial/svi001/app3/burmese-vocabulary/src/app/app.component.ts
+++ tutorial/svi001/app3/burmese-vocabulary/src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import { SmartAudio } from '../providers/smart-audio/smart-audio';
 
+
+import { timer } from 'rxjs/observable/timer';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -12,6 +15,8 @@ import { SmartAudio } from '../providers/smart-audio/smart-audio';
 export class MyApp {
   
   rootPage:any = TabsPage;
+
+  showSplash = true;
 
   constructor(
     platform: Platform, 
@@ -23,8 +28,22 @@ export class MyApp {
       platform.ready().then(() => {
         statusBar.styleDefault();
         splashScreen.hide();
+
+        timer(3000).subscribe(()=>this.showSplash = false);
+
         smartAudio.preload('001-ne-kaun-la', 'assets/audio/001-ne-kaun-la.mp3');
         smartAudio.preload('002-nameh', 'assets/audio/002-nameh.mp3');
+        smartAudio.preload('003-tweyada', 'assets/audio/003-tweyada.mp3');
+        smartAudio.preload('004-pudehnaw', 'assets/audio/004-pudehnaw.mp3');
+        smartAudio.preload('005-mapubabu', 'assets/audio/005-mapubabu.mp3');
+        smartAudio.preload('006-edehnaw', 'assets/audio/006-edehnaw.mp3');
+        smartAudio.preload('006-kaundehnaw', 'assets/audio/006-kaundehnaw.mp3');
+        smartAudio.preload('006-yadehnaw', 'assets/audio/006-yadehnaw.mp3');
+        smartAudio.preload('007-pudehnaw', 'assets/audio/007-pudehnaw.mp3');
+        smartAudio.preload('008-edehnaw', 'assets/audio/008-edehnaw.mp3');
+        smartAudio.preload('009-kaundehnaw', 'assets/audio/009-kaundehnaw.mp3');
+        smartAudio.preload('010-yadehnaw', 'assets/audio/010-yadehnaw.mp3');
+        smartAudio.preload('011-bazaga', 'assets/audio/011-bazaga.mp3');
       });
     }
 }
