d62397c7c3e2d5991ffd2337c7385c9f972a8a8c
Stephan Vits <stephan.vits@gmail.com>
2018-12-04 12:44:24 +0630

    initial commit advent calendar

---
+++ tutorial/svi001/app5/advent/src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AdventDaysProvider } from '../../providers/advent-days/advent-days';
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+    constructor(private adventDaysProvider: AdventDaysProvider) {
+ 
+    }
+ 
+    ionViewDidLoad(){
+        this.adventDaysProvider.loadDays();
+    }
+ 
+    flip(day){
+        this.adventDaysProvider.flipDay(day);
+    }
+ 
+}
