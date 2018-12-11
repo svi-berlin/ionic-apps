6427ffa7d4bd43cd412e8fdeb3bd226227940771
Stephan Vits <stephan.vits@gmail.com>
2018-12-12 05:48:16 +0630

    level 2 finished

--- tutorial/svi001/app9/animation/src/pages/l14/l14.ts
+++ tutorial/svi001/app9/animation/src/pages/l14/l14.ts
@@ -1,25 +1,71 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+// console.log
+import { L15Page } from '../l15/l15';
 
-/**
- * Generated class for the L14Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-l14',
   templateUrl: 'l14.html',
+  animations: [
+  	trigger('myvisibility', [
+  		state('visible', style({
+  			opacity: 1
+  		})),
+  		state('invisible', style({
+  			opacity: 0
+  		})),
+  		transition('* => *', animate('.5s'))
+  	])
+  ]
 })
+
 export class L14Page {
+	
+  visibleState: string   = 'visible'; 
+  applyClass: boolean    = false;
+  applyClass_1: boolean  = false;
+  applyClass_2: boolean  = false;
+  applyClass_3: boolean  = false;
+  applyClass_4: boolean  = false;
+  applyClass_5: boolean  = false;
+  applyClass_6: boolean  = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  falseAnswers: number = 0;
+
+  constructor(public navCtrl: NavController) {
+
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad L14Page');
+  giveAnswer(answer) {
+   if (answer == 1) {
+    if (this.applyClass_1 == false) {
+      this.applyClass_1 = true;
+      this.visibleState = 'invisible';
+      setTimeout(() => {
+            this.navCtrl.push(L15Page);
+        }, 1500);
+    } 
+   } else {
+    this.falseAnswers++;
+    if (this.falseAnswers > 2) {
+      this.falseAnswers = 0;
+      this.navCtrl.popToRoot();
+    }
+   }
+   if (answer == 2) {
+    this.applyClass_2 = true;
+   }
+   if (answer == 3) {
+    this.applyClass_3 = true;
+   }
+   if (answer == 4) {
+    this.applyClass_4 = true;
+   }
+  if (answer == 5) {
+    this.applyClass_5 = true;
+   }
+  if (answer == 6) {
+    this.applyClass_6 = true;
+   }
   }
-
 }
