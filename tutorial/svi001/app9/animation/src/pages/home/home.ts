bea27f286f4061c446ffa634b10a0636bf1c31bd
Stephan Vits <stephan.vits@gmail.com>
2018-12-11 19:26:44 +0630

    button color changing changed

--- tutorial/svi001/app9/animation/src/pages/home/home.ts
+++ tutorial/svi001/app9/animation/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { L1Page } from '../l1/l1';
+import { L2Page } from '../l2/l2';
 
 @IonicPage()
 @Component({
@@ -14,8 +15,13 @@ export class HomePage {
   	constructor(public navCtrl: NavController, public navParams: NavParams) {
   	}
 
-  	startTraining() {
-  		this.navCtrl.push(L1Page);
+  	startLevel(level: number) {
+  			if (level==1) {
+  				this.navCtrl.push(L1Page);	
+  			}
+  			if (level==2) {
+  				this.navCtrl.push(L2Page);	
+  			}
   	}
 
 }
